903f8dcc38613b7dced55307c3ef5a4d8cd4c500
matar <matart15@gmail.com>
2022-03-02 09:40:59 +0900

    strange error

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import { DataStore } from 'aws-amplify';
 
 import Amplify from 'aws-amplify';
 import { awsExports } from 'src/@app/libs/getAwsExports';
